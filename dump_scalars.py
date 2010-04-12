03ac7e1c40225bfc2dc43b12e5fbdf3efb632cee
kwmsmith <kwmsmith@gmail.com>
2010-04-11 20:09:55 -0500

    correct integer output in dump_scalars

--- dump_scalars.py
+++ dump_scalars.py
@@ -3,7 +3,7 @@ import tables
 
 def dump_scalars(fname, start=0):
     fields = ('index', 'basic_steps', 'wall_time', 'sim_time', 'be', 've', 'ne', 'msf')
-    formats = ('%-d %-d' + ' %-10.4g'*6).split()
+    formats = ('%8d %8d' + ' %-10.4g'*6).split()
     dta = tables.openFile(fname)
     scs = dta.getNode('/scalars/scalar_data')
     stop = scs.nrows
