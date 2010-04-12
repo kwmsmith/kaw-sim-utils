74cb22af7126cf2a7eb7888199b84b58dc8032a9
kwmsmith <kwmsmith@gmail.com>
2010-04-12 09:33:36 -0500

    added etot and some cleanup in dump_scalars.py

--- dump_scalars.py
+++ dump_scalars.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
 import tables
 
+INT_FMT, FLT_FMT = "%9d", "%#-13.4g"
+
+
 def dump_scalars(fname, start=0):
     fields = ('index', 'basic_steps', 'wall_time', 'sim_time', 'be', 've', 'ne', 'msf')
-    formats = ('%8d %8d' + ' %-10.4g'*6).split()
+
+    formats = (INT_FMT + ' ' + INT_FMT + (' ' + FLT_FMT)*6).split()
     dta = tables.openFile(fname)
     scs = dta.getNode('/scalars/scalar_data')
     stop = scs.nrows
     scal_dta = scs.read(start=start, stop=stop)
     dta.close()
     for row in scal_dta:
+        etot = row['be'] + row['ve'] + row['ne']
         for (field, fmt) in zip(fields, formats):
             print (fmt+' ') % (row[field]),
+        print FLT_FMT % etot,
         print
     return stop
         
