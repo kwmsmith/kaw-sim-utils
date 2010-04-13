9f8944152410b3b46e01ba397ca8e6340016a1ad
kwmsmith <kwmsmith@gmail.com>
2010-04-13 15:18:10 -0500

    add stats printout

--- dump_scalars.py
+++ dump_scalars.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 import tables
 
-INT_FMT, FLT_FMT = "%8d", "%#-10.4e"
+INT_FMT, FLT_FMT = ("%8d ",), ("% #-9.3e",)
 
 
 def dump_scalars(fname, start=0):
     fields = ('index', 'basic_steps', 'wall_time', 'sim_time', 'be', 've', 'ne', 'msf')
 
-    formats = (INT_FMT + ' ' + INT_FMT + (' ' + FLT_FMT)*6).split()
+    formats = INT_FMT*2 + FLT_FMT*6
     dta = tables.openFile(fname)
     scs = dta.getNode('/scalars/scalar_data')
     stop = scs.nrows
@@ -17,9 +17,20 @@ def dump_scalars(fname, start=0):
         etot = row['be'] + row['ve'] + row['ne']
         for (field, fmt) in zip(fields, formats):
             print (fmt+' ') % (row[field]),
-        print FLT_FMT % etot,
+        print FLT_FMT[0] % etot,
         print
     return stop
+
+def dump_stats(fname, field):
+    fields = ('index', 'ave', 'std_dev', 'skew', 'kurt')
+    formats = INT_FMT + FLT_FMT*4
+    dta = tables.openFile(fname)
+    stats = dta.getNode('/stats/%s' % field).read()
+    dta.close()
+    for row in stats:
+        for (field,fmt) in zip(fields, formats):
+            print (fmt+' ') % (row[field]),
+        print
         
 
 if __name__ == '__main__':
@@ -29,17 +40,22 @@ if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("-f", action="store_true", dest="follow",
                       default=False, help="follow, like 'tail -f'")
+    parser.add_option('-s', dest="stats", help="output statistics for field")
 
     options, args = parser.parse_args()
 
     fname = args[0]
 
-    start = dump_scalars(fname)
+    if options.stats:
+        dump_stats(fname, options.stats)
 
-    if options.follow:
-        while True:
-            try:
-                start = dump_scalars(fname, start)
-                time.sleep(10)
-            except KeyboardInterrupt:
-                import sys; sys.exit(0)
+    else:
+        start = dump_scalars(fname)
+
+        if options.follow:
+            while True:
+                try:
+                    start = dump_scalars(fname, start)
+                    time.sleep(10)
+                except KeyboardInterrupt:
+                    import sys; sys.exit(0)
